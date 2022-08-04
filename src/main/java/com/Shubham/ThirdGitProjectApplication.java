b62979d729f33295f8d2881d3308af61af6c0a85
gavalishubham94 <gavalishubham94@gmail.com>
2022-08-04 09:15:33 +0530

    third project added

---
+++ src/main/java/com/Shubham/ThirdGitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Shubham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdGitProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Shubham/ThirdGitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Shubham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdGitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
